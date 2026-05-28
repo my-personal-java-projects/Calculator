a356da0cc9306e1c73bb1e96b1767a329a919b05
unsigjhb025 <unsigjhb025@student.wethinkcode.co.za>
2026-05-28 14:07:49 +0200

    fix bug in main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
-/**
- * Entry point for the Calculator application.
- */
+//Entry point for the Calculator application.
+
+
 public class Main {
     public static void main(String[] args) {
         new CalculatorUI().start();
